e32eb5667d0ef373e491003129f44f19d4e7c975
WilfullMurder <81480788+WilfullMurder@users.noreply.github.com>
2021-06-29 11:14:47 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+ 
+
+namespace Reverse_Array
+{
+    public class ReverseArray
+    {
+        class Program
+        {
+            static void Main()
+            {
+                int i;
+                int n = 10;
+                double[] a = new double[100];
+
+                Console.Write("\n\n Please input 10 numbers to 2 decimal places. \n\n For Example: 1.00 \n\n");
+                for(i=0; i < n; i++)
+                {
+                    Console.Write("element - {0} : ", i);
+                    a[i] = Convert.ToDouble(Console.ReadLine()); //takes ten inputs
+                }
+
+                Console.Write("\n\n The values stored in the array are : \n "); // prints values in order
+                for(i=0; i < n; i++)
+                {
+                    Console.Write("{0} ", a[i]);
+                }
+
+                Console.Write("\n\n The values stored in the array in reverse order are : \n"); //prints values in reverse order
+                for(i=n-1; i>=0; i--)
+                {
+                    Console.Write("{0} ", a[i], "\n\n");
+                }
+                 
+                    }
+        }
+    }
+
+}
